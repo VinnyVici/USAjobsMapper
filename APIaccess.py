ffabec891334cfa8df6c1892997e507f41bb6495
VinnyVici <avin47@gmail.com>
2016-08-10 00:19:39 +0300

    Error with API URL
    When URL is changed to API with parameters error is returned 401 Not
    Authorized. Think I am still having issues just getting into the API.

--- APIaccess.py
+++ APIaccess.py
@@ -4,11 +4,17 @@ import requests
 #User-Agent: andrew@beta.build
 #Authorization-Key: 9Go7fDMdC1kl0+zjrWOOJ3417THZl5BaOisefF1TrU8=
 
-url = 'https://data.usajobs.gov'
+url = 'https://data.usajobs.gov/api/search?Keyword=Software'
 user = 'andrew@beta.build'
 key = '9Go7fDMdC1kl0+zjrWOOJ3417THZl5BaOisefF1TrU8='
+keyword = {'Keyword' : 'Ranger'}
 
 r = requests.get(url, auth=(user, key))
 
 print r.status_code
+print r.raise_for_status()
+print r.url
+print r.content
+
+
 

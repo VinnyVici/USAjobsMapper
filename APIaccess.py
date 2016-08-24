1b3482d2c698682d060df0c06f042f02fb3a5644
VinnyVici <VinnyVici@users.noreply.github.com>
2016-08-24 08:52:44 +0300

    Update APIaccess.py

--- APIaccess.py
+++ APIaccess.py
@@ -2,11 +2,11 @@ import requests
 
 #Host: data.usajobs.gov
 #User-Agent: andrew@beta.build
-#Authorization-Key: 9Go7fDMdC1kl0+zjrWOOJ3417THZl5BaOisefF1TrU8=
+#Authorization-Key: 
 
 url = 'https://data.usajobs.gov'
 user = 'andrew@beta.build'
-key = '9Go7fDMdC1kl0+zjrWOOJ3417THZl5BaOisefF1TrU8='
+key = 
 
 r = requests.get(url, auth=(user, key))
 
